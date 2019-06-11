836091779717c3b7cf034d2c73c2f595dd8906c0
Oleksii.Petrychenko <37137346+petrychenkodev@users.noreply.github.com>
2019-06-11 15:28:57 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -8,3 +8,33 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+let scores, roundScore, activePlayer;
+
+score = [0, 0];
+roundScore = 0;
+activePlayer = 1;
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function () {
+
+	//random number
+
+	let dice = Math.floor(Math.random() * 6) + 1;
+
+	//display the result
+
+	let diceDOM = document.querySelector('.dice');
+	diceDOM.style.display = 'block';
+	diceDOM.src = 'dice-' + dice + '.png';
+
+	//update the round score if the rolled number was not a 1
+
+	dice > 1 ? /*add score*/ : /*next player*/
+})
